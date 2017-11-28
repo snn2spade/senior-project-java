b9d3263f703bca305f7252209f880e283f237b45
Napat Pao <lostunevol@gmail.com>
2017-11-29 01:09:51 +0700

    Remove pass from DB config

--- src/settings/DatabaseConfig.java
+++ src/settings/DatabaseConfig.java
@@ -4,7 +4,7 @@ public class DatabaseConfig {
 	/* My SQL Database */
 	public static final String MYSQL_DB_NAME = "stock";
 	public static final String MYSQL_DB_USER = "root";
-	public static final String MYSQL_DB_PASS = "1049269";
+	public static final String MYSQL_DB_PASS = "";
 	
 	/* MongoDB Database */
 	public static final String MONGO_URI = "mongodb://localhost:27017";
