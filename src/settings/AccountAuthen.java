ebfd452be7179066f9ccf455d5915836919486b2
Napat Pao <lostunevol@gmail.com>
2017-11-29 01:14:22 +0700

    Remove cookie

--- src/settings/AccountAuthen.java
+++ src/settings/AccountAuthen.java
@@ -1,5 +1,5 @@
 package settings;
 
 public class AccountAuthen {
-	public static final String SETSMART_MAYBANK_AUTH_COOKIE = "_ga=GA1.3.119633372.1491640538; JSESSIONID=8552F60D16A86AA353D292F46C185BF3";
+	public static final String SETSMART_MAYBANK_AUTH_COOKIE = "_ga=GAX.X.XXXXXX.XXXXXXX; JSESSIONID=XXX";
 }
